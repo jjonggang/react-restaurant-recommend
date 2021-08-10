1598b52c2ee14781a453b33bee5e17f47385fa3b
JongkyeongPark <69239151+JongkyeongPark@users.noreply.github.com>
2021-08-11 00:02:28 +0900

    기본 프레임

---
+++ src/components/Restaurant.js
@@ -0,0 +1,61 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardMedia from '@material-ui/core/CardMedia';
+import CardContent from '@material-ui/core/CardContent';
+import Typography from '@material-ui/core/Typography';
+import { Link } from 'react-router-dom';
+import './Restaurant.css'
+
+const useStyles = makeStyles({
+    root: {
+      maxWidth: 345,
+    },
+    media: {
+      height: 140,
+    },
+  });
+  
+  function Restaurant({id, name, image, detail}) {
+  
+    const classes = useStyles();
+  
+    return (
+      <Link
+        to = {{
+          // eslint-disable-next-line no-template-curly-in-string
+          pathname: `/detail/${id}`,
+          state: {
+            id, 
+            name, 
+            image, 
+            detail
+          },
+        }}>
+        <article>
+          <Card className={classes.root} id = "cards" elevation = '0' variant="outlined">
+              <div>
+                {/* <img src = { image } alt = {name} /> */}
+                <CardMedia 
+                  id = "image"
+                  className={classes.media}
+                  image = {image}
+                  title= {name}
+                />
+                <CardContent id = "contents">
+                  <Typography gutterBottom variant="h5" component="h2">
+                    {name}
+                  </Typography>
+                  <Typography variant="body2" color="textSecondary" component="p">
+                    {detail}
+                  </Typography>
+
+                </CardContent>
+              </div>
+            </Card>
+        </article>
+      </Link>
+    );
+  }
+
+  export default Restaurant;
---
+++ src/routes/Detail.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import Container from '@material-ui/core/Container';
+
+class Detail extends React.Component{
+    render(){
+        const {location} = this.props;
+        return(
+            <div>
+                <h1>{location.state.id}</h1>
+                <h2>{location.state.name}</h2>
+                <h3>{location.state.detail}</h3>
+                <img src={location.state.image} alt = "음식 사진"/>
+            </div>
+        );
+    }
+}
+export default Detail;
+
+// function Detail(props){
+//     return(
+//         <span>{this.props.name}</span>
+//     );
+// }
+
+// export default Detail;
