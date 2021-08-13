4650d4f2c8a32389072ce05780991b4a934e83dc
JongkyeongPark <69239151+JongkyeongPark@users.noreply.github.com>
2021-08-13 16:10:33 +0900

    프레임

--- src/components/Restaurant.js
+++ src/components/Restaurant.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardMedia from '@material-ui/core/CardMedia';
@@ -9,7 +9,7 @@ import HelpIcon from '@material-ui/icons/Help';
 import ThumbUp from '@material-ui/icons/ThumbUp';
 import ThumbUpOutlined from '@material-ui/icons/ThumbUpOutlined';
 import { Link } from 'react-router-dom';
-import './Restaurant.css'
+import './Restaurant.css';
 
 const useStyles = makeStyles({
     root: {
@@ -20,54 +20,76 @@ const useStyles = makeStyles({
     },
   });
   
-  function Restaurant({id, name, image, detail}) {
-  
-    const classes = useStyles();
-  
-    return (
-     
-        <article>
-          <Card className={classes.root} id = "cards" elevation = '0' variant="outlined">
-              <div>
-                {/* <img src = { image } alt = {name} /> */}
-                <CardMedia 
-                  id = "image"
-                  className={classes.media}
-                  image = {image}
-                  title= {name}
-                />
-                <CardContent className = "parent">
-                  <div className = "child">
-                    <Typography gutterBottom variant="h5" component="h2">
-                      {name}
-                    </Typography>
-                    <Typography variant="body2" color="textSecondary" component="p">
-                      {detail}
-                    </Typography>
-                  </div>
-                  <div className = "child" id = "likeButton">   
-                    <div><Button variant="contained" color="primary" id="button1">{<ThumbUpOutlined/>}</Button></div>
-                    <div>
-                      <Link
-                        to = {{
-                          // eslint-disable-next-line no-template-curly-in-string
-                          pathname: `/detail/${id}`,
-                          state: {
-                            id, 
-                            name, 
-                            image, 
-                            detail
-                          },
-                        }}>
-                        <Button variant="contained" color="primary" id="button2">{<HelpIcon/>}</Button>
-                      </Link>                  
-                    </div>
-                  </div>
-                </CardContent>
-              </div>
-            </Card>
-        </article>
-    );
-  }
 
-  export default Restaurant;
+function Restaurant({id, name, image, detail}) {
+  const [likeIcon, setLikeIcon] = useState(false);
+
+  const changeLikeIcon = () => {
+    setLikeIcon(!likeIcon);
+  };
+
+  // const gotoDetail = () => {
+  //   <Detail id name image detail/>
+  // };
+
+  const classes = useStyles();
+
+  return (
+    
+      <article>
+        <Card className={classes.root} id = "cards" elevation = '0' variant="outlined">
+            <div>
+              {/* <img src = { image } alt = {name} /> */}
+              <CardMedia 
+                id = "image"
+                className={classes.media}
+                image = {image}
+                title= {name}
+              />
+              <CardContent className = "parent">
+                <div className = "child">
+                  <Typography id = "restaurantName" gutterBottom variant="h5" component="h2">
+                    {name}
+                  </Typography>
+                  <Typography variant="body2" color="textSecondary" component="p">
+                    {detail}
+                  </Typography>
+                </div>
+                <div className = "child" id = "likeButton">   
+                  <div onClick = {changeLikeIcon}>
+                    {likeIcon? (
+                      <Button variant="contained" color="primary" id="button1">
+                       {<ThumbUp/>}
+                      </Button>
+                      ) : 
+                      (
+                      <Button variant="contained" color="primary" id="button1">
+                        {<ThumbUpOutlined/>}
+                      </Button>
+                      )
+                    }
+                  </div>
+                  <div>
+                    <Link
+                      to = {{
+                        // eslint-disable-next-line no-template-curly-in-string
+                        pathname: `/detail/${id}`,
+                        state: {
+                          id, 
+                          name, 
+                          image, 
+                          detail
+                        },
+                      }}>
+                      <Button variant="contained" color="primary" id="button2">{<HelpIcon/>}</Button>
+                    </Link>                  
+                  </div>
+                </div>
+              </CardContent>
+            </div>
+          </Card>
+      </article>
+  );
+}
+
+export default Restaurant;
